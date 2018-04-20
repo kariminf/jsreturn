e142c0d5e256a92cfd7a5900d5811aafbf3c1f47
kariminf <kariminfo0@gmail.com>
2018-04-20 17:52:41 +0100

    the birth of jsreturn

---
+++ jsreturn.js
@@ -0,0 +1,114 @@
+
+(function() {
+
+  "use strict";
+
+  let JsReturn = {};
+
+  if ( typeof module === "object" && module && typeof module.exports === "object" ) {
+    module.exports = JsReturn;
+  }
+  else {
+    window.JsLingua.Cls.Info = JsReturn;
+  }
+
+  //{code: string}, {cond: string, ret: string}
+  let cache = [];
+  let current = {};
+  let vars = {};
+  let varsL = "";
+  let def = "";
+
+  let eif = false;
+  let letvar = "";
+
+  function repVars(val) {
+    return val.replace(new RegExp( "(^| )(" + varsL + ")([ ;]|$)", "g"), "$1vars.$2$3");
+  }
+
+  function toCache() {
+    cache.push(current);
+    current = {};
+    eif = false;
+  }
+
+  JsReturn.let = function(v) {
+    letvar = v + " = ";
+    if(varsL) varsL += "|";
+    varsL += v;
+    return JsReturn;
+  };
+
+  JsReturn.be = function(val) {
+    letvar += val + ";";
+    JsReturn.code(letvar);
+    letvar = "";
+    return JsReturn;
+  };
+
+  JsReturn.as = function(val) {
+    letvar += val;
+    JsReturn.code(letvar);
+    letvar = "";
+    return JsReturn;
+  };
+
+  JsReturn.if = function (cond) {
+    if (eif) {
+      current.ret = "";
+      toCache();
+    }
+
+    eif = true;
+
+    current.cond = repVars(cond);
+
+    return JsReturn;
+  };
+
+  JsReturn.ret = function (ret) {
+    if (! eif) return JsReturn;
+
+    current.ret = repVars(ret);
+
+    toCache();
+
+    return JsReturn;
+
+  };
+
+  JsReturn.code = function (code) {
+    if (eif) {
+      current.ret = "";
+      toCache();
+    }
+
+    current.code = repVars(code);
+    toCache();
+
+    return JsReturn;
+
+  };
+
+  JsReturn.default = function (ret) {
+    def = ret;
+  };
+
+
+
+  JsReturn.eval = function () {
+    for(let i = 0; i < cache.length; i++) {
+      let item = cache[i];
+      if (item.code) eval(item.code);
+      else if (item.cond) {
+        if (eval(item.cond)) return eval(item.ret);
+      }
+    }
+
+    return eval(def);
+
+  };
+
+
+
+}());
---
+++ unit.js
@@ -0,0 +1,43 @@
+
+function func(val) {
+  let b = 5;
+  if (typeof val === "string") return val;
+  if (val === 0) return "zero";
+
+  let a = val - 1;
+
+  if (a === 0) return "one";
+
+  if (a === b) return "Our fixed value minus one";
+
+  return "value is " + val;
+}
+
+function func2(val) {
+  let jsreturn = require("./jsreturn");
+
+  jsreturn.let('val').be(val)
+          .let("b").be(5)
+          .if('typeof val === "string"').ret('val')
+          .if('val === 0').ret('"zero"')
+          .let('a').as('val - 1')
+          .if('a === 0').ret('"one"')
+          .if('a === b').ret('"Our fixed value minus one"')
+          .default('"value is " + val');
+
+  return jsreturn.eval();
+
+}
+
+const args = process.argv;
+let val = parseInt(args[2]);
+
+let hrstart = process.hrtime();
+console.log(func(val));
+let hrend = process.hrtime(hrstart);
+console.info("Normal: %ds %dms", hrend[0], hrend[1]/1000000);
+
+hrstart = process.hrtime();
+console.log(func2(val));
+hrend = process.hrtime(hrstart);
+console.info("JsReturn: %ds %dms", hrend[0], hrend[1]/1000000);
